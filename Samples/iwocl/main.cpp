6fafcb6cacbf04af70ce68cbc691216f59e82a9d
juanchuletas <juan.garcia.cpp@gmail.com>
2026-04-18 23:08:53 -0600

    adding samples

---
+++ Samples/iwocl/main.cpp
@@ -0,0 +1,80 @@
+#include "funGT/fungt.hpp"
+const unsigned int SCREEN_WIDTH = 2100;
+const unsigned int SCREEN_HEIGHT = 1200;
+
+
+
+int main() {
+    //std::string path = findProjectRoot();
+    //Path to your shaders and models:
+    ModelPaths model_ball, model_lamp, model_hamm, iwocl;
+
+    model_lamp.path = getAssetPath("Obj/LuxoLamp/Luxo.obj");
+    // model_ball.path = getAssetPath("Obj/LuxoBall/luxoball.obj");
+    // model_hamm.path = getAssetPath("Obj/Hamm/Hamm.obj");
+    iwocl.path = getAssetPath("Obj/iwocl/iwocl.obj");
+    //Creates a FunGT Scene to display 
+    FunGTScene myGame = FunGT::createScene(SCREEN_WIDTH, SCREEN_HEIGHT);
+    //Background color, use 255.f for pure white, 
+    myGame->setBackgroundColor();
+
+    //Initializes the Graphics Stuff
+    myGame->initGL();
+    //Gets an instance of the SceneManager class to render objects
+    FunGTSceneManager scene_manager = myGame->getSceneManager();
+    // Creates an animation object
+    FunGTSModel iwocl_model = SimpleModel::create();
+    iwocl_model->load(iwocl);
+    iwocl_model->position(0.f, 0.f, 0.f);
+    iwocl_model->rotation(0.f, 0.f, 0.f);
+    iwocl_model->scale(5.0);
+    FunGTSModel pixarLamp = SimpleModel::create();
+
+    // // Loads Pixar lamp data
+    pixarLamp->load(model_lamp);
+    pixarLamp->position(0.f, 0.f, -10.f);
+    pixarLamp->rotation(0.f, 0.f, 0.f);
+
+    // FunGTSModel pixarBall = SimpleModel::create();
+
+    // pixarBall->load(model_ball);
+    // pixarBall->position(0.f, 2.3f, 10.f);
+    // pixarBall->rotation(0.f, 0.f, 0.f);
+    // pixarBall->scale(2.3);
+    // FunGTSModel hamm = SimpleModel::create();
+    // hamm->load(model_hamm);
+    // hamm->position(10.f, 0.f, 0.f);
+    // hamm->rotation(0.f, 45.f, 0.f);
+    // hamm->scale(0.03f);
+    FunGTSGeom ground = SimpleGeometry::create(Geometry::Plane);
+    ground->load(getAssetPath("img/floor.png"));
+    ground->position(0.0,0.0,0.0);
+
+
+    FunGTSGeom backWall = SimpleGeometry::create(Geometry::Plane);
+    backWall->load(getAssetPath("img/leftwall.png"));
+    backWall->rotation(90.0f, 0.0f, 0.0f);
+    backWall->position(0.0f, 0.0f, -40.0f);
+    FunGTSGeom left_Wall = SimpleGeometry::create(Geometry::Plane);
+    left_Wall->load(getAssetPath("img/leftwall.png"));
+    left_Wall->rotation(90.0f, 0.0f, -90.0f);
+    left_Wall->position(-40.0f, 0.0f, 0.0f);
+
+    myGame->set([&]() { // Sets up all the scenes in your game
+        // Adds the renderable objects to the SceneManager
+                // Adds the renderable objects to the SceneManager
+        scene_manager->addRenderableObj(ground);
+        scene_manager->addRenderableObj(backWall);
+        scene_manager->addRenderableObj(left_Wall);
+        scene_manager->addRenderableObj(iwocl_model);
+        scene_manager->addRenderableObj(pixarLamp);
+        //scene_manager->addRenderableObj(pixarBall);
+        //scene_manager->addRenderableObj(hamm);
+        });
+    myGame->render([&]() {
+
+        });
+
+    return 0;
+
+}
